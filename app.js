b1064cb9227664a72e0b41a2d8addfaf688590a7
Wouter <woutermalan7909@gmail.com>
2022-06-10 15:57:26 +0200

    added a work list and extra header and footer files to use as templates

--- app.js
+++ app.js
@@ -16,20 +16,38 @@ let options = {
     month: 'long'
 };
 let day = today.toLocaleDateString("en-US", options);
-res.render('list', {kindOfDay: day, newItems: items});
+res.render('list', {listTitle: day, newItems: items});
 
 });
 let items= [];
+let workItems= [];
 
 app.post('/', (req, res) => {
     let item = req.body.newItem;
+    if(req.body.list === 'Work'){
+        workItems.push(item);
+        res.redirect('/work');
+    }else{
+        items.push(item);
+        res.redirect('/');
+    }
+});
 
-    items.push(item);
-
-    res.redirect('/');
+app.get('/work', (req, res) => {
+res.render('list', {listTitle: 'Work List', newItems: workItems});
 
 });
 
+app.post('/work', (req, res) => {
+    let item = req.body.newItem;
+
+    workItems.push(item);
+
+    res.redirect('/work');
+
+
+
+});
 
 
 app.listen(3000, () => {
